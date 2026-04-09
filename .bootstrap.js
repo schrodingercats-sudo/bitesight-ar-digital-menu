fa5dea16b0b4e17977f7e11e401540c4209ee8d0
Vibesdk <vibesdk-bot@cloudflare.com>
2026-04-09 10:29:03 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add @google/model-viewer@^3.5.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
